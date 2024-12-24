dde2d2637ac73b112a3bed832025060ed77cae04
yangcong1993 <36957499+yangcong1993@users.noreply.github.com>
2024-12-24 15:46:25 +0800

    Update config.ts
    个人信息更新

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE = {
-  title: '潮流周刊',
-  author: 'Tw93',
-  description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
+  title: 'Trendblogger',
+  author: 'YC',
+  description: '记录CAE工程师的学习与生活',
   keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pake,Swift,设计',
   icon: 'https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png',
   pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
