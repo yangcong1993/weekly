cdc021a6cbe99fd8d794a22132602c6004f84ef7
yangcong1993 <1310018013@qq.com>
2024-12-24 21:31:14 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const SITE = {
   icon: 'https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png',
   pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
   homePage: 'https://weekly.tw93.fun',
-  blogPage: 'https://tw93.fun/',
+  blogPage: 'https://astroblog-swart.vercel.app/',
   twitterId: 'HiTw93',
   githubId: 'yangcong1993',
   repo: 'yangcong1993/weekly',
