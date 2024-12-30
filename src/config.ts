84ad9836346a3a0886f1e69beb17d2b551522a99
yangcong1993 <36957499+yangcong1993@users.noreply.github.com>
2024-12-30 20:24:02 +0800

    name change

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const SITE = {
-  title: 'Trendblogger',
+  title: 'Trendbloger',
   author: 'YC',
   description: '记录CAE工程师的学习与生活',
-  keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pake,Swift,设计',
+  keywords: 'CAE,Ansys,Blog,静力学',
   icon: 'https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png',
   pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
   homePage: 'https://www.trendbloger.cn/',
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -14,9 +14,9 @@ export function GET() {
   posts = posts.slice(0, 12);
 
   return rss({
-    title: "潮流周刊",
-    description: "记录工程师 Tw93 的不枯燥生活",
-    site: "https://weekly.tw93.fun/",
+    title: "TrendWeekly",
+    description: "记录CAE工程师的学习生活",
+    site: "https://www.trendbloger.cn/",
     customData: `<image><url>https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png</url></image><follow_challenge><feedId>41147805276726275</feedId><userId>42909600318350336</userId></follow_challenge>`,
     items: posts.map((item) => {
       const [issueNumber, issueTitle] = item.url.split("/posts/")[1].split("-");
