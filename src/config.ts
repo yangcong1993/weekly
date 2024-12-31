a5d12543a6ca4f47552acd9ff8dfba3b3a81da57
yangcong1993 <36957499+yangcong1993@users.noreply.github.com>
2024-12-31 15:54:55 +0800

    post-删除

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ export const SITE = {
   pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
   homePage: 'https://www.trendbloger.cn/',
   blogPage: 'https://www.trendbloger.top/',
-  twitterId: 'HiTw93',
+  Bilibili: '2584848',
   githubId: 'yangcong1993',
   repo: 'yangcong1993/weekly',
-  bilibiliId: '2584848',
+
 };
